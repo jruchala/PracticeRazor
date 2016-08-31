520d4932888023db05edf2047f99e83d5467addd
jruchala <jruchala@gmail.com>
2016-08-31 15:18:10 -0400

    created pages to practice Razor code and to demonstrate razor, JavaScript and jQuery functions

---
+++ PracticeRazor/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PracticeRazor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "\n\nThis is my first ViewBag practice. \n<br/>Does a line break work?";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult PostTest()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult AddNumbers()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+
+        }
+        public ActionResult ReverseAStringWithRazor()
+        {
+            
+
+            return View();
+
+        }
+    }
+}
